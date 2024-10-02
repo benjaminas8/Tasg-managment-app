3138c0dc8272fa9b491d6fe464e640b97bad55b6
Benjaminas Čeikauskas <benasceik@gmail.com>
2024-10-02 17:27:25 +0300

    added validation function for future comments section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,37 @@ import Header from "../components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Modal from "@/components/Modal/Modal";
 import axios from "axios";
+import cookie from "js-cookie";
 import TasksWrapper from "@/components/TasksWrapper/TasksWrapper";
 import { Task as TaskType } from "@/types/tasks";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   const [isModalOpen, setModalOpen] = useState(false);
   const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  const jwt = cookie.get("task_manager_app_jwt");
+
+  const validateUser = async () => {
+    try {
+      const headers = {
+        authorization: jwt,
+      };
+
+      const response = await axios.get("http://localhost:3003/login/validate", {
+        headers,
+      });
+
+      if (response.status !== 200) {
+        router.push("/login");
+      }
+    } catch (err) {
+      console.log(err);
+      router.push("/login");
+    }
+  };
 
   const fetchTasks = async () => {
     try {
@@ -23,6 +48,12 @@ export default function Home() {
   };
 
   useEffect(() => {
+    if (!jwt) {
+      router.push("/login");
+    }
+
+    validateUser();
+
     fetchTasks();
   }, []);
 
